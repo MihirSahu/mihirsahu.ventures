d7248fc109f4e6bd59028d17ce062694d3eb75ba
Mihir Sahu <2002mihir@gmail.com>
2023-11-11 21:14:28 -0600

    Center text

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-        <div className='text-3xl'>Mihir Sahu</div>
-        <div>Making something people want.</div>
+        <div className='text-center text-3xl'>Mihir Sahu</div>
+        <div className='text-center'>Making something people want</div>
     </main>
   )
 }
